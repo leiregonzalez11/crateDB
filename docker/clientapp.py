ffde07e44b5c476beebdb0679d1900be1f8f96bc
leire <leiregonzalezlopez99@gmail.com>
2021-12-10 16:20:39 +0100

    Union de docker (cliente simple  + volumenes)

--- docker/clientapp.py
+++ docker/clientapp.py
@@ -1,5 +1,8 @@
-import sys
 import time
+import os.path as path
+import csv
+from datetime import datetime
+from random import random
 
 from crate import client
 
@@ -8,7 +11,7 @@ def createDatabase(connection):
     cursor = connection.cursor()
 
     try:
-        cursor.execute("CREATE TABLE coches (id integer PRIMARY KEY, marca text, modelo text, anio int)")
+        cursor.execute("CREATE TABLE coches (id integer primary key, marca text, modelo text, anio int)")
         print("TABLE CREATED")
 
     except Exception as err:
@@ -21,6 +24,7 @@ def insertStatements(connection):
     cursor = connection.cursor()
 
     try:
+
         print("Sleeping for 10 seconds...")
         time.sleep(10)
 
@@ -47,35 +51,47 @@ def insertStatements(connection):
 
         print("INSERT ERROR: %s" % err)
 
-def registroCoches():
+def guardarDatos(result):
 
-    time.sleep(10)
+    with open('../docker-volumes/datos_db.txt', 'w') as fich:
+        fich.write(str(datetime.now()) + '\n')
+        for element in result:
+            fich.write("Registro añadido a la BD: " + str(element) + '\n')
+
+
+def registroCoches():
 
     #Creamos la conexión
 
     try:
-        connection = client.connect("http://localhost:4200/", username="crate", timeout=5, error_trace=True, backoff_factor=0.2)
+        print ("Trying to connect to database...")
+        connection = client.connect("http://localhost:4200/", username="crate", timeout=10, error_trace=True,
+                                    backoff_factor=0.2)
         print("CONNECTION DONE")
 
     except Exception as err:
         print("CONNECT ERROR: %s" % err)
 
+    #Eliminamos la tabla, si existiera, para no crear conflicto:
 
-    createDatabase(connection) #Creamos una tabla donde insertar los datos
-    insertStatements(connection) #Insertamos varios registros en la tabla creada
+    cursor = connection.cursor()
+    cursor.execute("DROP TABLE IF EXISTS coches;")
 
-    #Probamos que los datos se han insertado correctamente:
+    createDatabase(connection)  # Creamos una tabla donde insertar los datos
+    insertStatements(connection)  # Insertamos varios registros en la tabla creada
+
+    # Probamos que los datos se han insertado correctamente:
 
     try:
         print("Sleeping for 10 seconds...")
         time.sleep(10)
         cursor = connection.cursor()
-        print("\n El número de elementos en la BD debería ser 5")
+        print("\nEl número de elementos en la BD debería ser 5")
         cursor.execute("SELECT COUNT(*) FROM coches")
         result = cursor.fetchone()
         print("Y el numero de elementos es: " + str(result))
-        print(" Los datos incluidos son: \n")
-        cursor.execute("SELECT id,marca,modelo,anio FROM coches order by id")
+        print("\nLos datos incluidos son: \n")
+        cursor.execute("SELECT id, marca, modelo, anio FROM coches order by id")
         result = cursor.fetchall()
         for element in result:
             print("\t" + str(element) + "\n")
@@ -85,6 +101,14 @@ def registroCoches():
 
         print("\n No se han podido obtener los datos \n")
 
+    # Guardamos los datos en un fichero
+    print("Guardando los registros en un fichero...")
+    guardarDatos(result)
+    print("Registros guardados\n")
+
+    print("Esto ha sido todo. Agur :) \n")
+
+    time.sleep(5)
 
 if __name__ == "__main__":
 
